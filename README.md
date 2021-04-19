675d9c3932231bf47584958e50f3b925d8ded73c
Joel Keyser <keyser.joel@gmail.com>
2021-04-18 21:39:33 -0500

    Implement

---
+++ README.md
@@ -0,0 +1,25 @@
+# kindle-notes-parser
+Takes notes file(s) exported from Kindle and parses them by color.
+
+I wanted this because each color of highlight means something different for me, and for certain colors, I like to view those notes across all books, rather than scoped to a specific book. This script _does_ parse specific to each book, but in a format that's easy to take all the highlights of a color and paste them into another document for cross-book viewing pleasures. The format (* [highlight]; ** [note]) was particularly chosen because it's easy to paste into OneNote, at which point, the [One Markdown](http://www.onenotegem.com/a/documents/gem-for-OneNote/Review_Tab/2019/1126/1214.html) add-in can be used to format all of these into bullets.
+
+# requirements
+1. [ruby](https://www.ruby-lang.org/en/downloads/)
+2. [html-to-text](https://www.npmjs.com/package/html-to-text)
+
+# usage
+```
+Usage: ruby kindle-notes-parser.rb [-f file path|-d directory path] -o [output directory path]
+    -f, --file=file                  Path to file that contains kindle-exported notes
+    -d, --directory=directory        Path to directory of files that contain kindle-exported notes
+    -o, --output=OUTPUT              Output path to directory where parsed notes by book will go
+
+Example: ruby kindle-notes-parser.rb -f /raw_exported/harry_potter.html -o /parsed
+  creates:
+/parsed/harry_potter/blue.txt
+/parsed/harry_potter/orange.txt
+/parsed/harry_potter/pink.txt
+/parsed/harry_potter/yellow.txt
+
+Export kindle notes by going into your notebook for a book and clicking share->export notebook
+```
---
+++ kindle-notes-parser.rb
@@ -0,0 +1,66 @@
+require 'active_support/inflector'
+require 'optparse'
+
+# format of notes within file looks like this:
+# Highlight (yellow) - Page 12
+# [highlighted text, all on one line]
+# Note - Page 12 [notes are optional!]
+# [noted text, all on one line]
+def parse_highlight_color(color, output_book_path, output_html_stripped_path)
+  highlight_count = `cat #{output_html_stripped_path} | awk '/\\(#{color}\\)/{print}' | wc -l`.to_i
+
+  print_count = "BEGIN{print \"highlight count: #{highlight_count}\"}"
+  print_notes = 'getline; print "* " $0; getline; if ($0 ~ /Note -/) { getline; print "** " $0 }'
+  cmd_parse_notes = "awk '#{print_count}; /\\(#{color}\\)/{#{print_notes}}'"
+  output_color_path = File.join(output_book_path, "#{color}.txt")
+
+  `cat #{output_html_stripped_path} | #{cmd_parse_notes} > #{output_color_path}`
+end
+
+def parse_kindle_note(file_path, output_path)
+  file_name = File.basename(file_path, ".*")
+  output_book_path = File.join(output_path, file_name)
+  `mkdir -p #{output_book_path}`
+  output_html_stripped_path = File.join(output_book_path, "html_stripped.txt")
+
+  # wordwrap=0 so that parsing doesn't split lines for word wrapping
+  `html-to-text --wordwrap=0 < #{file_path} > #{output_html_stripped_path}`
+
+  ["blue", "orange", "pink", "yellow"].each do |color|
+    parse_highlight_color(color, output_book_path, output_html_stripped_path)
+  end
+end
+
+def parse_kindle_notes(options)
+  file_paths = options[:file] ? [options[:file]] : Dir[File.join(options[:directory], "*")]
+
+  file_paths.each do |file_path|
+    parse_kindle_note(file_path.shellescape, options[:output].shellescape)
+  end
+end
+
+option_parser = OptionParser.new do |opts|
+  opts.banner = "Usage: ruby kindle-notes-parser.rb [-f file path|-d directory path] -o [output directory path]"
+
+  opts.on("-f", "--file=file", "Path to file that contains kindle-exported notes")
+  opts.on("-d", "--directory=directory", "Path to directory of files that contain kindle-exported notes")
+  opts.on("-oOUTPUT", "--output=OUTPUT", "Output path to directory where parsed notes by book will go")
+
+  opts.separator(
+"\nExample: ruby kindle-notes-parser.rb -f /raw_exported/harry_potter.html -o /parsed
+  creates:
+/parsed/harry_potter/blue.txt
+/parsed/harry_potter/orange.txt
+/parsed/harry_potter/pink.txt
+/parsed/harry_potter/yellow.txt"
+  )
+
+  opts.separator("\nExport kindle notes by going into your notebook for a book and clicking share->export notebook")
+end
+
+options = {}
+option_parser.parse!(into: options)
+raise "must pass a file path or a directory path, and not both" if !(options[:file].present? ^ options[:directory].present?)
+
+parse_kindle_notes(options)
+
